665f1aabd4093ad4785ddc32b99e506f765b1be9
mamamudang <humlake7.66@gmail.com>
2024-12-17 17:56:38 +0700

    lab10_2.cpp completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,11 +6,20 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	// เพิ่มข้อความบรรทัดแรก
+	dest << "-------------------- BOOM ---------------------\n";
+	
+	// คัดลอกข้อมูลจาก cheerbook.txt
+	string line;
+	while (getline(source, line)) {
+		dest << line << "\n";
+	}
+	
+	// เพิ่มข้อความบรรทัดสุดท้าย
+	dest << "-------------------- HA!! ---------------------\n";
 
     source.close();
     dest.close();
